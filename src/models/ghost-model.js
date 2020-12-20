8b58b11a645ce05c8cd0a14ebb017a796e7252d9
Moritz Laube <info@moritz-laube.de>
2020-12-20 22:58:51 +0100

    Turn Ghost class into mongoose Schema

--- src/models/ghost-model.js
+++ src/models/ghost-model.js
@@ -1,36 +1,58 @@
 const { v4: uuidv4 } = require('uuid')
+const mongoose = require('mongoose')
 
-class Ghost {
-  id = uuidv4()
+const GhostSchema = mongoose.Schema({
+  email: String,
+  firstName: String,
+  lastName: String,
+  proMembership: Boolean,
+  profession: String,
+  mobilePhone: Number,
+  profilePicture: String,
+  aboutText: String,
+  timezoneOffset: Number,
+  categories: [],
+  languages: [],
+  blockedDates: [{
+    startBlockedDate: Date,
+    endBlockedDate: Date
+  }],
+  portfolio: [],
+  ratings: [],
+  requests: [],
+  createdAt: { type: Date, default: Date.now }
+})
+// class Ghost {
+//   id = uuidv4()
 
-  #proMembership = false
+//   #proMembership = false
 
-  constructor(email, firstName, lastName, profession, mobilePhone) {
-    this.email = email
-    this.firstName = firstName
-    this.lastName = lastName
-    this.profession = profession
-    this.mobilePhone = mobilePhone
-    this.profilePicture = ''
-    this.aboutText = ''
-    this.categories = []
-    this.languages = []
-    this.blockedDates = []
-    this.portfolio = []
-    this.ratings = []
-    this.requests = []
-    this.accountCreated = Date.now()
+//   constructor(email, firstName, lastName, profession, mobilePhone) {
+//     this.email = email
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.profession = profession
+//     this.mobilePhone = mobilePhone
+//     this.profilePicture = ''
+//     this.aboutText = ''
+//     this.categories = []
+//     this.languages = []
+//     this.blockedDates = []
+//     this.portfolio = []
+//     this.ratings = []
+//     this.requests = []
+//     this.accountCreated = Date.now()
 
-    this.initTimezoneOffset()
-  }
+//     this.initTimezoneOffset()
+//   }
 
-  get proMembership() {
-    return this.#proMembership
-  }
+//   get proMembership() {
+//     return this.#proMembership
+//   }
 
-  initTimezoneOffset() {
-    this.timezoneOffset = new Date().getTimezoneOffset() / 60
-  }
-}
+//   initTimezoneOffset() {
+//     this.timezoneOffset = new Date().getTimezoneOffset() / 60
+//   }
+// }
 
-module.exports = Ghost
+module.exports = mongoose.model('Ghost', GhostSchema)
