e91f46e4c3d3a1522bf88a19a744524182c95922
Moritz Laube <info@moritz-laube.de>
2020-12-22 22:58:34 +0100

    Add Ghost.find() query

--- src/routes/ghosts.js
+++ src/routes/ghosts.js
@@ -4,21 +4,27 @@ const Ghost = require('../models/ghost-model')
 const router = express.Router()
 
 // init test data start
-const ghosts = []
 
-const moritz = new Ghost({
-  email: 'ml@moritzlaube.com',
-  firstName: 'Moritz',
-  lastName: 'Laube'
-})
 
-moritz.save().then()
+// const maria = new Ghost({
+//   email: 'ko@mariala.com',
+//   firstName: 'Maria',
+//   lastName: 'Cordou',
+//   proMembership: false,
+//   occupation: 'Ghost & Moodscout',
+//   mobliePhone: '+491703345233',
+//   categories: ['Tabletop', 'Beauty'],
+//   blockedDates: [{startBlockedDate: '2019-09-07', endBlockedDate: '2019-10-01'}, {startBlockedDate: '2020-09-07', endBlockedDate: '2020-10-01'}]
+// })
+
+// maria.save().then(() => console.log('contact saved'))
 
 // init test data end
 
 
 /* GET ghosts listing. */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const ghosts = await Ghost.find()
   res.send(ghosts)
 })
 
