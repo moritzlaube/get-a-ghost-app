d5da8c3e5250b9a3d0851cae9315611d46af0cb2
Moritz Laube <info@moritz-laube.de>
2020-12-22 23:39:01 +0100

    Add User.find() query

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,15 +5,44 @@ const Ghost = require('../models/ghost-model')
 const router = express.Router()
 
 // init test data start
-const users = []
 
+// const michael = new User({
+//   email: 'mchael@burnham.com',
+//   firstName: 'Michael',
+//   lastName: 'Burnham',
+//   company: 'USS Discovery',
+//   mobilePhone: '+1234565788',
+//   profilePicture: 'https://www.gcloud.com/profile'
+// })
 
+// const alva = new User({
+//   email: 'alva@gmail.com',
+//   firstName: 'Alva',
+//   lastName: 'Bocca',
+//   company: 'Embassy',
+//   mobilePhone: '+1234565788',
+//   profilePicture: 'https://www.gcloud.com/profile'
+// })
+
+// const axel = new User({
+//   email: 'axel@freese.com',
+//   firstName: 'Axel',
+//   lastName: 'Freese',
+//   company: 'Lidl',
+//   mobilePhone: '+1234565788',
+//   profilePicture: 'https://www.gcloud.com/profile'
+// })
+
+// axel.save()
+// alva.save()
+// michael.save()
 
 // init test data end
 
 
 /* GET users listing. */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const users = await User.find()
   res.send(users)
 })
 
