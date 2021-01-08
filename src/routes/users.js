f3510b853c408528504d722e48785305fd2e6b27
moritzlaube <39345259+moritzlaube@users.noreply.github.com>
2021-01-08 17:29:37 +0100

    Add dedicated initialize route for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,40 +3,37 @@ const User = require('../models/user-model')
 
 const router = express.Router()
 
-// init test data start
+// init test data
+router.get('/init', async (req, res) => {
+  await User.create({
+    email: 'axel@freese.com',
+    firstName: 'Axel',
+    lastName: 'Freese',
+    company: 'Lidl',
+    mobilePhone: '+1234565788',
+    profilePicture: 'https://www.gcloud.com/profile',
+  })
 
-// const danny = new User({
-//   email: 'mchael@burnham.com',
-//   firstName: 'Michael',
-//   lastName: 'Burnham',
-//   company: 'USS Discovery',
-//   mobilePhone: '+1234565788',
-//   profilePicture: 'https://www.gcloud.com/profile'
-// })
+  await User.create({
+    email: 'alva@gmail.com',
+    firstName: 'Alva',
+    lastName: 'Bocca',
+    company: 'Embassy',
+    mobilePhone: '+1234565788',
+    profilePicture: 'https://www.gcloud.com/profile',
+  })
 
-// const sophie = new User({
-//   email: 'alva@gmail.com',
-//   firstName: 'Alva',
-//   lastName: 'Bocca',
-//   company: 'Embassy',
-//   mobilePhone: '+1234565788',
-//   profilePicture: 'https://www.gcloud.com/profile'
-// })
+  await User.create({
+    email: 'mchael@burnham.com',
+    firstName: 'Michael',
+    lastName: 'Burnham',
+    company: 'USS Discovery',
+    mobilePhone: '+1234565788',
+    profilePicture: 'https://www.gcloud.com/profile',
+  })
 
-// const markus = new User({
-//   email: 'axel@freese.com',
-//   firstName: 'Axel',
-//   lastName: 'Freese',
-//   company: 'Lidl',
-//   mobilePhone: '+1234565788',
-//   profilePicture: 'https://www.gcloud.com/profile'
-// })
-
-// markus.save()
-// sophie.save()
-// danny.save()
-
-// init test data end
+  res.send('Users initialized')
+})
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
