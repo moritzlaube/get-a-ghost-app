adac1703fc8ddbae22e081d0ad0d4912d1df5ca6
Moritz Laube <info@moritz-laube.de>
2021-07-21 16:52:42 +0200

    Work on querying ghosts

--- api/src/controllers/ghost.controller.js
+++ api/src/controllers/ghost.controller.js
@@ -1,15 +1,39 @@
 const Ghost = require('../models/ghost.model')
+const { isWithinInterval, parseISO } = require('date-fns')
 
 exports.getAllGhosts = async (req, res) => {
   let ghosts
 
   try {
+    // if no query params present, get all ghosts
     if (Object.keys(req.query).length === 0 && req.query.constructor === Object) {
       ghosts = await Ghost.find()
     } else {
-      const { type, start, end } = req.query
-      console.log(start) // undefined
-      ghosts = await Ghost.find().where('type').in([type])
+      //otherwise use query params to query db
+      const { type, startDate, endDate, language } = req.query
+      if (typeof type === 'string') {
+        if (type === 'ghostwriter' && language !== undefined) {
+          ghosts = await Ghost.find().where('type').in([type]).where('language').in([language])
+        } else {
+          ghosts = await Ghost.find().where('type').in([type])
+        }
+      } else {
+        ghosts = await Ghost.find()
+          .where('type')
+          .in([...type])
+      }
+
+      if (startDate && endDate) {
+        // check if startDate && endDate are within blocked date range and filter ghosts accordingly
+        ghosts = ghosts.filter(ghost => {
+          return ghost.blocked.every(({ start, end }) => {
+            return (
+              !isWithinInterval(parseISO(startDate), { start, end }) &&
+              !isWithinInterval(parseISO(endDate), { start, end })
+            )
+          })
+        })
+      }
     }
 
     return res.status(200).json({ ok: true, data: ghosts })
@@ -58,11 +82,96 @@ exports.updateGhost = async (req, res) => {
         ghost[key] = req.body[key]
       }
     })
-    // ghost.blocked.push(...req.body.blocked)
+
     await ghost.save()
 
     return res.status(200).json({ ok: true, data: ghost })
   } catch (error) {
     return res.status(500).json({ ok: false, error })
   }
+}
+
+exports.init = async (req, res) => {
+  await Ghost.insertMany([
+    {
+      type: ['ghostwriter', 'moodscout'],
+      name: {
+        first: 'Andrea',
+        last: 'Schrul',
+      },
+      ghostName: 'Schrulchen',
+      language: ['de', 'en'],
+      blocked: [
+        {
+          start: new Date(2021, 1, 10),
+          end: new Date(2021, 1, 13),
+        },
+        {
+          start: new Date(2021, 2, 14),
+          end: new Date(2021, 2, 21),
+        },
+        {
+          start: new Date(2021, 3, 21),
+          end: new Date(2021, 3, 28),
+        },
+        {
+          start: new Date(2021, 5, 1),
+          end: new Date(2021, 5, 12),
+        },
+      ],
+    },
+    {
+      type: ['ghostwriter'],
+      name: {
+        first: 'Fintan',
+        last: 'Gsänger',
+      },
+      language: ['de'],
+      blocked: [
+        {
+          start: new Date(2021, 3, 22),
+          end: new Date(2021, 4, 5),
+        },
+        {
+          start: new Date(2021, 5, 22),
+          end: new Date(2021, 6, 3),
+        },
+        {
+          start: new Date(2021, 7, 1),
+          end: new Date(2021, 7, 8),
+        },
+        {
+          start: new Date(2021, 8, 8),
+          end: new Date(2021, 8, 22),
+        },
+      ],
+    },
+    {
+      type: ['moodscout'],
+      name: {
+        first: 'Kai',
+        last: 'Werner',
+      },
+      language: [],
+      blocked: [
+        {
+          start: new Date(2021, 3, 22),
+          end: new Date(2021, 4, 5),
+        },
+        {
+          start: new Date(2021, 5, 22),
+          end: new Date(2021, 6, 3),
+        },
+        {
+          start: new Date(2021, 9, 8),
+          end: new Date(2021, 9, 12),
+        },
+        {
+          start: new Date(2021, 9, 19),
+          end: new Date(2021, 10, 22),
+        },
+      ],
+    },
+  ])
+  res.status(200).json({ ok: true, message: 'Ghost docs created!' })
 }
