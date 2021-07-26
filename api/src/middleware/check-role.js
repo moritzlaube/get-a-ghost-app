fde57df6ad68ad34c16a214b0451579c94ed6e11
moritzlaube <info@moritz-laube.de>
2021-07-26 16:05:56 +0200

    Refactor

--- api/src/middleware/check-role.js
+++ api/src/middleware/check-role.js
@@ -1,5 +1,4 @@
 module.exports = role => (req, res, next) => {
-  if (!req.user) return next({ status: 401, message: 'Unauthorized' })
   if (req.user.roleModel !== role) return next({ status: 401, message: 'Unauthorized' })
   return next()
 }
