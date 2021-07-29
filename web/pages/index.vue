877b255b348fd1d0401819832c3909da97f102b1
moritzlaube <info@moritz-laube.de>
2021-07-29 18:13:17 +0200

    Change index

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,7 +1,15 @@
-<template>
-  <Tutorial/>
+<template lang="pug">
+div
+  h1 Das ist ein Test!
+  BaseGhostLogo
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+.logo {
+  margin-top: 2rem;
+}
+</style>
