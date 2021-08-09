785ba49d1ee701eb626ecc585a696aa678f1e3a2
Moritz Laube <info@moritz-laube.de>
2021-08-09 15:33:41 +0200

    Add a BaseButton component

---
+++ web/components/base/Button.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  button(type="submit")
+    slot
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: var(--clr-pink);
+  color: black;
+  box-shadow: 5px 5px 12px var(--shadow-dark),
+    -5px -5px 12px var(--shadow-light);
+  border-radius: 5px;
+  border: none;
+  width: 100%;
+  padding: var(--space-sm) var(--space-md);
+  line-height: 1;
+}
+</style>
