8df2af4b017f09b45a47b90a59aa88c2f5b8d413
Moritz Laube <info@moritz-laube.de>
2021-08-09 15:34:41 +0200

    Delete index route and shift everything to login route

---
+++ web/pages/login.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+div.container.login-page
+  div.flow
+    BaseGhostLogo.mt-md.center-align
+    p Login and find an available Moodscout and/or Ghostwriter within seconds. No more chain calling your handwritten, out-of-date list of Ghosts.
+  form
+    fieldset.flow
+      BaseInput(type="email" id="email" name="email" v-model="email" placeholder="Email" label="Email")
+      BaseInput(type="password" id="password" name="password" v-model="password" placeholder="Password" label="Password")
+    BaseButton.mt-xxl LOG IN
+    p.mt-md.center-align No account yet? Register to get started right away and get 30 days for free.
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.login-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-content: flex-start;
+  height: 100vh;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
