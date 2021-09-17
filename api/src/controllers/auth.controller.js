50d00ecbf2eb1c2dc34ce4ce1d34b5e2f91faea6
Moritz Laube <info@moritz-laube.de>
2021-09-17 22:19:40 +0200

    Update auth controller

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,25 +1,36 @@
 /* eslint-disable no-underscore-dangle */
 const jwt = require('jsonwebtoken')
 const Account = require('../models/account.model')
+const User = require('../models/user.model')
 const Token = require('../models/token.model')
 const authService = require('../services/auth.service')
 
 exports.login = async (req, res) => res.status(200).json({ ok: true, data: req.user })
 
 exports.register = async (req, res) => {
-  const { email, password } = req.body
+  const { email, password, company, name } = req.body
 
   try {
     // create Account with barebones info.
     const createdAccount = await Account.register(
       {
         email,
+        roleModel: 'User',
         verificationToken: authService.getRandomInt(1000, 9999),
         verificationTokenExpire: new Date(Date.now() + 1000 * 3600 * 24),
       },
       password
     )
     // TODO: send verificationToken to email via Sendgrid
+
+    const createdUser = await User.create({
+      account: createdAccount._id,
+      name,
+      company,
+    })
+
+    createdAccount.role = createdUser._id
+    await createdAccount.save()
 
     return req.login(createdAccount, err => {
       if (err) throw new Error(err)
@@ -50,6 +61,10 @@ exports.logout = (req, res) => {
   } catch (err) {
     return res.status(500).json({ ok: false, data: err })
   }
+}
+
+exports.getUser = (req, res) => {
+  return res.status(200).json({ ok: true, data: req.user })
 }
 
 exports.sendInvite = async (req, res) => {
@@ -84,7 +99,12 @@ exports.verifyInvite = async (req, res) => {
     const hasAccount = await Account.exists({ email })
     const hasToken = await Token.exists({ token })
     if (!hasToken || hasAccount)
-      return res.status(404).json({ ok: false, message: 'Wrong Token or Account already exists' })
+      return res.status(404).json({ ok: false, message: 'Wrong Token or Account with that email already exists' })
+
+    const token = await Account.find({ token })
+    token.verfied = true
+    await token.save()
+
     return res.status(200).json({ ok: true, data: { email }, message: 'Token verified' })
   } catch (error) {
     return res.status(401).json({ ok: false, data: error })
