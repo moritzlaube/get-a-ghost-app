ac177a5698a54d36d76cdfbfc3abfd011bda54c2
Moritz Laube <info@moritz-laube.de>
2021-09-27 09:13:49 +0200

    Add BaseSelect Component

---
+++ web/components/base/Select.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+    div
+        label.sr-only(:for="id") {{ label }}
+        div.select
+            select(:id="id")
+                slot
+</template>
+
+<script>
+export default {
+  name: 'BaseSelect',
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: 'Label',
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    id: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+// Reset select element
+select {
+  // A reset of styles, including removing the default dropdown arrow
+  appearance: none;
+  // Additional resets for further consistency
+  border: none;
+  background-color: transparent;
+  margin: 0;
+  width: 100%;
+  font-family: inherit;
+  font-size: inherit;
+  cursor: inherit;
+  line-height: inherit;
+  color: inherit;
+}
+
+select::-ms-expand {
+  display: none;
+}
+
+select:focus {
+  outline: none;
+}
+
+// style select element
+.select {
+  display: flex;
+  width: 100%;
+  box-shadow: 5px 5px 12px var(--shadow-dark),
+    -5px -5px 12px var(--shadow-light);
+  border-radius: 10px;
+  padding: var(--space-xs) 35px var(--space-xs) var(--space-sm);
+  background-image: url('@/assets/images/arrow-down.svg');
+  background-repeat: no-repeat;
+  background-position: right var(--space-sm) top 50%;
+  cursor: pointer;
+  color: var(--text-mid-grey);
+}
+</style>
