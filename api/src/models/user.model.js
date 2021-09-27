7b5863ce3c495a9d5ec45ce07d8c77f99505f474
Moritz Laube <info@moritz-laube.de>
2021-09-27 09:12:32 +0200

    Add virtuals to get initials

--- api/src/models/user.model.js
+++ api/src/models/user.model.js
@@ -2,22 +2,29 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const userSchema = new Schema({
-  account: {
-    type: Schema.Types.ObjectId,
-    ref: 'Account',
+const userSchema = new Schema(
+  {
+    account: {
+      type: Schema.Types.ObjectId,
+      ref: 'Account',
+    },
+    name: {
+      first: String,
+      last: String,
+    },
+    company: String,
+    phone: String, // E.164 format -> npm package 'phone'
+    address: {
+      street: String,
+      zip: String,
+      country: String,
+    },
   },
-  name: {
-    first: String,
-    last: String,
-  },
-  company: String,
-  phone: String, // E.164 format -> npm package 'phone'
-  address: {
-    street: String,
-    zip: String,
-    country: String,
-  },
+  { toObject: { virtuals: true }, toJSON: { virtuals: true } }
+)
+
+userSchema.virtual('initials').get(function () {
+  return this.name.first.charAt(0).toUpperCase() + this.name.last.charAt(0).toUpperCase()
 })
 
 module.exports = mongoose.model('User', userSchema)
