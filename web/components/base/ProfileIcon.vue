b35be3ce8b147533f6b26a2493d991036b58cee4
Moritz Laube <info@moritz-laube.de>
2021-09-27 09:13:38 +0200

    Add ProfileIcon with Initials

---
+++ web/components/base/ProfileIcon.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+    div.profile
+        div.profile__initials
+            slot
+</template>
+
+<script>
+export default {
+  name: 'ProfileIcon',
+}
+</script>
+
+<style lang="scss" scoped>
+.profile {
+  display: flex;
+  background-color: var(--bg-dark-grey);
+  box-shadow: 3px 3px 8px var(--shadow-dark), -3px -3px 8px var(--shadow-light);
+  border-radius: 50%;
+  border: 1px solid var(--mid-grey);
+  color: var(--text-light);
+  font-weight: var(--fw-bold);
+  font-size: var(--text-lg);
+  width: 3rem;
+  height: 3rem;
+  line-height: 0.5;
+}
+
+.profile__initials {
+  margin: auto;
+}
+</style>
