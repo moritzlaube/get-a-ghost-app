241e4a01ccf12eb8dd6c96757e7b3cdd4314e1c6
Moritz Laube <info@moritz-laube.de>
2021-09-27 09:14:50 +0200

    Work on multistep registering form

--- web/pages/register.vue
+++ web/pages/register.vue
@@ -2,58 +2,74 @@
 div.container.register-page
   div.flow
     BaseGhostLogo.mt-md.center-align
-    p Login and find an available Moodscout and/or Ghostwriter within seconds. No more chain calling your handwritten, out-of-date list of Ghosts.
-  form(@submit.prevent="userLogin")
-    fieldset.flow
-      BaseInput(type="text" id="fname" name="fname" v-model="register.name.first" placeholder="First Name" label="First Name")
-      BaseInput(type="text" id="lname" name="lname" v-model="register.name.last" placeholder="Last Name" label="Last Name")
-      BaseInput(type="text" id="company" name="company" v-model="register.company" placeholder="Company (optional)" label="Company")
-      BaseInput(type="email" id="email" name="email" v-model="register.email" placeholder="Email" label="Email")
-      BaseInput(type="password" id="password" name="password" v-model="register.password" placeholder="Password" label="Password")
-    BaseButton.mt-xxl REGISTER
-    p.mt-md.center-align You already have an account? #[NuxtLink(to="/login") Login] instead.
+    p(v-if="currentStep === 1") Register and find an available Moodscout and/or Ghostwriter within seconds. No more chain calling your handwritten, out-of-date list of Ghosts.
+    p(v-if="currentStep === 2") Please let us know your name, company (if applicable) and phone. Your phone will come in handy when your requested Ghost needs to get in contact with you.
+  div   
+    FormRegisterEmailAndPassword(v-if="currentStep === 1" @update="processStep" @next="currentStep++")
+    FormRegisterNameAndCompany(v-if="currentStep === 2" @update="processStep" @sendForm="registerUser" :class="{ loading: isLoading }")
+    FormVerifyPIN(v-if="currentStep === 3" @update="processStep" @verify-pin="verifyPin" :class="{ loading: isLoading }")
+    p.mt-md.center-align(v-if="currentStep === 1") You already have an account? #[NuxtLink(to="/login") Login] instead.
 </template>
 
 <script>
 export default {
   name: 'RegisterPage',
-  middleware: 'guest',
   auth: false,
   data() {
     return {
-      register: {
-        name: {
-          first: '',
-          last: '',
-        },
-        company: '',
-        email: '',
-        password: '',
-      },
-      error: '',
+      form: {},
       response: '',
+      isLoading: false,
+      currentStep: 1,
     }
   },
   methods: {
-    async userLogin() {
+    processStep(stepData) {
+      Object.assign(this.form, stepData)
+    },
+    async registerUser() {
+      this.isLoading = true
+
       try {
         const { data } = await this.$axios.post('auth/register', {
-          ...this.register,
+          ...this.form,
         })
 
         this.response = data.data
 
         await this.$auth.loginWith('local', {
           data: {
-            email: this.register.email,
-            password: this.register.password,
+            email: this.form.email,
+            password: this.form.password,
           },
         })
 
-        // this.$router.push('/')
+        this.currentStep++
       } catch (err) {
-        this.error = err.response.data
+        this.response = err.response.data
       }
+
+      this.isLoading = false
+    },
+    async verifyPin() {
+      this.isLoading = true
+
+      try {
+        const { data } = await this.$axios.post('auth/verify', {
+          pin: this.form.code,
+        })
+
+        this.response = data.data
+      } catch (err) {
+        this.response = err.response.data
+      }
+
+      this.isLoading = false
+
+      // refetch user to get email verification status
+      await this.$auth.fetchUser()
+
+      this.$router.push('/')
     },
   },
 }
@@ -64,12 +80,6 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  align-content: flex-start;
   height: 100vh;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
 }
 </style>
