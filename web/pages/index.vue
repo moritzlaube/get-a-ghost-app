45a2dfeebb6f013def12ec2711bedcd988284d07
Moritz Laube <info@moritz-laube.de>
2021-09-27 09:14:13 +0200

    Start working on search form

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,10 +1,70 @@
 <template lang="pug">
   div
-    h1 Das ist die Homepage
+    header
+      div.container
+        BaseGhostLogo(style="width: 65%")
+        BaseProfileIcon(v-if="isAuthenticated") {{ loggedInUser.userData.initials }}
+    div.container
+      p Find all the availabe Ghosts for a given timeframe. If your dates are flexible, please check the appropriate box.
+      
+      form(@submit.prevent="onSubmit")
+        div
+          p Find me a
+          BaseSelect(label="Find Ghostwriter or Moodscout")
+            option(disabled selected) Select profession ...
+            option Ghostwriter
+            option Moodcscout
+            option Both
+
+        div
+          p writing in
+          BaseSelect(label="Select language")
+            option(disabled selected) Select language ...
+            option german
+            option english
+
+        div
+          p available on
+
+      pre 
+        code {{isAuthenticated}}
+        br
+        code {{loggedInUser}}
+      button(@click="logout") Logout
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'Homepage',
+  auth: false,
+  data() {
+    return {
+      form: {},
+    }
+  },
+  computed: {
+    ...mapGetters(['isAuthenticated', 'loggedInUser']),
+  },
+  methods: {
+    async onsubmit() {},
+    async logout() {
+      await this.$auth.logout()
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+header {
+  box-shadow: 0 12px 12px -2px var(--shadow-dark);
+  margin-bottom: var(--space-sm);
+}
+
+header > div {
+  display: flex;
+  justify-content: space-between;
+  padding: var(--space-xl) var(--space-sm);
+}
+</style>
