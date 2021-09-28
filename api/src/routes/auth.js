0c4e8a8221fe1cc6e90a6895d0f7ff32493b5e93
moritzlaube <info@moritz-laube.de>
2021-09-28 17:56:24 +0200

    Delete isAuthenticated middleware

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 const passport = require('passport')
 const authController = require('../controllers/auth.controller')
 const usersController = require('../controllers/user.controller')
-const isAuthenticated = require('../middleware/is-authenticated')
 
 router.post('/login', passport.authenticate('local'), usersController.getUser)
 router.post('/register', authController.registerUser)
