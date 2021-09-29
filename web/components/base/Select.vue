f525579e87e3c811d1f3667feac0aeec0838b01b
moritzlaube <info@moritz-laube.de>
2021-09-29 13:39:29 +0200

    Update BaseSelect

--- web/components/base/Select.vue
+++ web/components/base/Select.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
   div
     label.sr-only(:for="id") {{ label }}
-    div.select(:tabindex="tabindex" :placeholder="placeholder" @blur="open = false" @click="open = !open" :id="id")
+    div.select(ref="select" :tabindex="tabindex" :placeholder="placeholder" :class="{ 'select--open': open }" @blur="open = false" @click="open = !open" :id="id")
       div.flex
-        div.select__selected(:class="{ 'select--open': open }") {{ selected_option ? selected_option : placeholder }}
-        <svg width="10" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.8.8 5 4.8 1.2.8 0 2l5 5.2L10 2 8.8.8Z" fill="#C4C4C4"/></svg>
-      div.options(:class="{ 'select--hide': !open }")
-        div(v-for="(option, i) in options" :key="i" @click="selected_option = option; $emit('input', option)") {{ option }}
+        div.select__selected {{ selected_option ? selected_option : placeholder }}
+        div.arrow(:class="{ open: open }")
+          <svg width="10" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.8.8 5 4.8 1.2.8 0 2l5 5.2L10 2 8.8.8Z" fill="#C4C4C4"/></svg>
+      transition(name="expand" @before-enter="beforeEnter" @enter="enter" @leave="leave")
+        div.options(v-if="open")
+          div(v-for="(option, i) in options" :key="i" :class="{ selected: option === selected_option }" @click="selected_option = option; $emit('input', {[id]: option})") {{ option }}
 </template>
 
 <script>
@@ -38,7 +40,20 @@ export default {
     return {
       open: false,
       selected_option: null,
+      collapsedElementHeight: null,
     }
+  },
+  methods: {
+    beforeEnter(el) {
+      this.$refs.select.style.height = this.$refs.select.offsetHeight + 'px'
+      this.element = this.$refs.select.offsetHeight
+    },
+    enter(el) {
+      this.$refs.select.style.height = this.$refs.select.scrollHeight + 'px'
+    },
+    leave(el) {
+      this.$refs.select.style.height = this.element + 'px'
+    },
   },
 }
 </script>
@@ -49,40 +64,56 @@ export default {
   box-shadow: 5px 5px 12px var(--shadow-dark),
     -5px -5px 12px var(--shadow-light);
   border-radius: 10px;
-  border: 1px solid red;
+  border: 1px solid var(--mid-grey);
   background-repeat: no-repeat;
   background-position: right var(--space-sm) top 50%;
   cursor: pointer;
   color: var(--text-mid-grey);
+  overflow: hidden;
+  will-change: height;
+  transition: height 0.25s ease-out;
 }
 
 .select__selected {
   padding: var(--space-xs);
 
-  & + svg {
+  & + .arrow {
     display: block;
     margin: var(--space-xs);
+    transition: transform 0.25s ease-in-out;
+  }
+
+  & + .arrow.open {
+    transform: rotate(180deg);
   }
 }
 
 .select .options {
   background-color: var(--bg-dark-grey);
-  border-radius: 10px;
-  max-height: 500px;
 }
 
 .select .options > * {
   padding: 0.25rem var(--space-xs);
 }
 
-.select .options.select--hide {
-  max-height: 0;
-  overflow: hidden;
+.select .options > *:hover,
+.select .options > .selected {
+  background-color: var(--mid-grey);
 }
 
 .flex {
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+
+.expand-enter-active,
+.expand-leave-active {
+  transition: opacity 0.25s;
+}
+
+.expand-enter,
+.expand-leave-to {
+  opacity: 0;
 }
 </style>
