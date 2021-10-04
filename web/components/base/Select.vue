d971113f2439cea376337b3f2fb95b697bef773c
Moritz Laube <info@moritz-laube.de>
2021-10-04 10:19:38 +0200

    Update Select

--- web/components/base/Select.vue
+++ web/components/base/Select.vue
@@ -4,7 +4,7 @@
     .select(ref="select" :tabindex="tabindex" :placeholder="placeholder" :class="{ 'select--open': open }" @blur="open = false" @click="open = !open" :id="id")
       .flex
         .select__selected {{ selected_option ? selected_option : placeholder }}
-        dv.arrow(:class="{ open: open }")
+        .arrow(:class="{ open: open }")
           <svg width="10" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.8.8 5 4.8 1.2.8 0 2l5 5.2L10 2 8.8.8Z" fill="#C4C4C4"/></svg>
       transition(name="expand" @before-enter="beforeEnter" @enter="enter" @leave="leave")
         .options(v-if="open")
