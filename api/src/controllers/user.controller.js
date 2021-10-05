71ce91c2e878c721f2475fae6ef61a79108cbfaa
moritzlaube <info@moritz-laube.de>
2021-10-05 19:12:18 +0200

    Add API route to update user NOT FINISHED

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,21 +1,34 @@
+const User = require('../models/user.model')
 /* eslint-disable no-underscore-dangle */
 exports.getUser = (req, res) => {
-  const user = {
-    account_id: req.user._id,
-    email: req.user.email,
-    emailVerified: req.user.emailVerified,
-    userData: {
-      user_id: req.user.profile._id,
-      name: req.user.profile.name,
-      initials: req.user.profile.initials,
-      company: req.user.profile.company,
-      phone: req.user.profile.phone,
-      address: req.user.profile.address,
-    },
-  }
+  // const user = {
+  //   account_id: req.user._id,
+  //   email: req.user.email,
+  //   emailVerified: req.user.emailVerified,
+  //   userData: {
+  //     user_id: req.user.profile._id,
+  //     name: req.user.profile.name,
+  //     initials: req.user.profile.initials,
+  //     company: req.user.profile.company,
+  //     phone: req.user.profile.phone,
+  //     address: req.user.profile.address,
+  //   },
+  // }
 
   res.status(200).json({
     ok: true,
-    data: user,
+    data: req.user,
   })
+}
+
+exports.updateUser = async (req, res) => {
+  // destructure info from frontend and then mutate Object by user[key] = value
+  const newUserInfo = req.body
+  const user = await User.findById(req.user.profile.id)
+
+  const updatedUser = Object.assign(user, newUserInfo)
+
+  // const savedUser = await user.save()
+
+  res.status(200).json({ ok: true, data: updatedUser })
 }
--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 const isAuthenticated = require('../middleware/is-authenticated')
 const userController = require('../controllers/user.controller')
 
+// GET active user
 router.get('/me', isAuthenticated, userController.getUser)
+
+// UPDATE active user
+router.put('/me', isAuthenticated, userController.updateUser)
 
 module.exports = router
