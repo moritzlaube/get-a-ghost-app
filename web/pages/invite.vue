cd030fccb1080b2419892d72a3796d705a23dea7
Moritz Laube <info@moritz-laube.de>
2021-10-11 12:46:27 +0200

    Use fetch instead of asyncData and log error object

--- web/pages/invite.vue
+++ web/pages/invite.vue
@@ -1,37 +1,30 @@
 <template lang="pug">
   div.container
-    p Hi! {{error}}
+    p(v-if="$fetchState.pending") Loading ...
+    p(v-else-if="error") {{error.message}}
 </template>
 
 <script>
 export default {
   name: 'VerifyAndOnboard',
   auth: false,
-  async asyncData({ query, $axios }) {
-    const { token } = query
-    let response
-    try {
-      response = await $axios.get(`/auth/invite/${token}`)
-    } catch (error) {
-      return {
-        error,
-      }
-    }
-
-    return {
-      email: response.data.email,
-    }
-  },
   data() {
     return {
+      email: null,
       error: null,
-      response: null,
     }
   },
-  // async fetch() {
-  //   const { token } = this.$route.query
-  //   email = await this.$axios.$get(`/auth/invite/${token}`)
-  // },
-  // fetchOnServer: false,
+  async fetch() {
+    const { token } = this.$route.query
+    try {
+      const { data } = await this.$axios.$get(`/auth/invite/${token}`)
+      this.email = data
+    } catch (error) {
+      this.error = {
+        message: error.response.data.message,
+        status: error.response.status,
+      }
+    }
+  },
 }
 </script>
