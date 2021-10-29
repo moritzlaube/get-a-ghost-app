45abcb46126b1c26e6a6f52e2e2989a706e3f3c0
moritzlaube <info@moritz-laube.de>
2021-10-29 13:33:56 +0200

    Add comments made by Armagan

--- api/src/models/request.model.js
+++ api/src/models/request.model.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+
 const { Schema } = mongoose
 
 const requestSchema = new Schema(
   {
+    // remove word request
     requestedBy: {
       type: Schema.Types.ObjectId,
       ref: 'User',
