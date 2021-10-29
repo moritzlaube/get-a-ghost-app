e32c163488641b6616d9435b486c5af73c395d72
moritzlaube <info@moritz-laube.de>
2021-10-29 13:39:53 +0200

    Add error page

---
+++ web/layouts/error.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div.container.center-align
+    h1(v-if="error.statusCode === 404") Page Not Found
+    h1(v-else) An error occured.
+    NuxtLink(to="/") Go back home
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage',
+  props: {
+    error: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
