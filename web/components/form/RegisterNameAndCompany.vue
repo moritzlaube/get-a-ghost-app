04fbc9abfc104799c030e857478b04b720bc174f
Moritz Laube <info@moritz-laube.de>
2021-11-04 16:29:39 +0100

    Add countryCode to register page

--- web/components/form/RegisterNameAndCompany.vue
+++ web/components/form/RegisterNameAndCompany.vue
@@ -5,12 +5,16 @@
         BaseInput(type="text" id="fname" name="fname" v-model="form.name.first" placeholder="First Name" label="First Name" required)
         BaseInput(type="text" id="lname" name="lname" v-model="form.name.last" placeholder="Last Name" label="Last Name" required)
       BaseInput(type="text" id="company" name="company" v-model="form.company" placeholder="Company (optional)" label="Company")
-      BaseInput(type="tel" id="phone" name="phone" v-model="form.phone" placeholder="+491511234567" label="Phone" required)
+      .split
+        BaseSearchSelect.country-code(type="tel" id="country-code" v-model="form.countryCode" :options="Array.from(countryCodes)" :pre-selected="'+49'" required)
+        BaseInput(type="tel" id="phone" name="phone" v-model="form.phone" placeholder="1511234567" label="Phone" required)
       slot
     BaseButton(type="submit" :disabled="!(form.name.first && form.name.last && form.phone)").mt-xxl.has-shadow REGISTER
 </template>
 
 <script>
+import countryCodes from '@/assets/json/country-codes.json'
+
 export default {
   name: 'FormRegisterNameAndCompany',
   data() {
@@ -21,15 +25,22 @@ export default {
           last: '',
         },
         company: '',
+        countryCode: '',
         phone: '',
       },
     }
+  },
+  computed: {
+    countryCodes() {
+      return new Set(countryCodes.map((code) => code.dial_code))
+    },
   },
   methods: {
     onFormInput() {
       this.$emit('update', {
         name: this.form.name,
         company: this.form.company,
+        countryCode: this.form.countryCode,
         phone: this.form.phone,
       })
     },
@@ -44,5 +55,8 @@ export default {
 form {
   display: flex;
   flex-direction: column;
+}
+.country-code {
+  flex-basis: 30%;
 }
 </style>
