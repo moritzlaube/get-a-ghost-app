71f8a2bab035d502b7086ca313e525e2d430a6a3
Moritz Laube <info@moritz-laube.de>
2021-11-05 17:43:35 +0100

    Create new route to resend pin

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -3,6 +3,7 @@ const passport = require('passport')
 const authController = require('../controllers/auth.controller')
 const usersController = require('../controllers/user.controller')
 const isAuthenticated = require('../middleware/is-authenticated')
+const checkRole = require('../middleware/check-role')
 
 router.post('/login', passport.authenticate('local'), usersController.getUser)
 router.post('/register', authController.registerUser)
@@ -10,5 +11,6 @@ router.post('/verify', isAuthenticated, authController.verifyToken)
 router.get('/logout', authController.logout)
 router.post('/invite', authController.sendInvite)
 router.get('/invite/:token', authController.verifyInvite)
+router.get('/resend-token', isAuthenticated, checkRole('User'), authController.resendEmailVerificationToken)
 
 module.exports = router
