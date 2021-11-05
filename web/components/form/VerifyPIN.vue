5cb62a496cbefef926374cfa6dc074132dd3346f
Moritz Laube <info@moritz-laube.de>
2021-11-05 17:44:47 +0100

    Send new pin request to backend

--- web/components/form/VerifyPIN.vue
+++ web/components/form/VerifyPIN.vue
@@ -2,6 +2,9 @@
   div.flow
     div.vue-pincode-wrapper
         PincodeInput(v-model="code" placeholder="·" @input="onFormInput")
+    p No verificytion code? Just click the 
+      button.btn(type="button" @click="sendVerificationCode") link
+      | &nbsp;and have a new PIN sent to you.
     BaseButton(type="button" @click="onSubmit").has-shadow VERIFY
 </template>
 
@@ -14,6 +17,25 @@ export default {
     }
   },
   methods: {
+    async sendVerificationCode() {
+      try {
+        await this.$axios.$get('/auth/resend-token')
+        this.$notify({
+          type: 'success',
+          title: "We've sent you an email",
+          text: 'We just have sent you an email with your new pin. Please also check your spam.',
+          duration: 5000,
+        })
+      } catch (errors) {
+        const errorResponse = this.$errorHandler.setAndParse(errors)
+        this.$notify({
+          type: 'error',
+          title: 'Error: ' + errorResponse.status,
+          text: errorResponse.message,
+          duration: 5000,
+        })
+      }
+    },
     onFormInput() {
       this.$emit('update', {
         code: this.code,
@@ -42,5 +64,15 @@ input.vue-pincode-input {
   border-radius: 5px;
   border: none;
   color: var(--text-light);
+}
+
+.btn {
+  color: var(--clr-pink);
+  text-decoration: underline;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  border: 0;
+  background-color: transparent;
 }
 </style>
