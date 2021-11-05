7eb4e2fa431f4a179664c2b14f37b9dfedc64dae
Moritz Laube <info@moritz-laube.de>
2021-11-05 20:24:58 +0100

    Exchange hardcoded email address with env variable

--- api/src/services/mail.service.js
+++ api/src/services/mail.service.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 module.exports = async (email, subject, html) => {
   const msg = {
     to: email,
-    from: 'ml@moritzlaube.com',
+    from: process.env.SG_EMAIL,
     subject,
     html,
   }
