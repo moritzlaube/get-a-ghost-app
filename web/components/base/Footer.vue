f35a39ab1b58b1a15dca10e012d896873cf191ad
Moritz Laube <info@moritz-laube.de>
2021-11-08 21:56:19 +0100

    Update footer with link to toc

--- web/components/base/Footer.vue
+++ web/components/base/Footer.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
   footer
-    .container.center-align
-      a(href='/privacy') Privacy Statement
-      p.footer__subline
+    .container.flow
+      div.footer-split
+        NuxtLink(to="/privacy") Privacy Statement
+        NuxtLink(to="/terms-and-conditions") Terms &amp; Conditions
+      p.footer__subline.center-align
         | Made with ❤️ by Dots &amp; Dashes &copy; {{ new Date().getFullYear() }}
       
 </template>
@@ -21,5 +23,12 @@ footer {
 .footer__subline {
   font-size: var(--text-xs);
   color: var(--text-mid-grey);
+}
+
+.footer-split {
+  display: flex;
+  max-width: 35ch;
+  justify-content: space-between;
+  margin: 0 auto;
 }
 </style>
